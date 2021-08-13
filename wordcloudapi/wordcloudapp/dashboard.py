c45e0785ef4014c3399494432dd14559fb86b257
Nayomi Chibana <nchibana@Nayomis-MacBook-Pro.local>
2021-08-13 02:43:30 -0700

    small dataset

--- wordcloudapi/wordcloudapp/dashboard.py
+++ wordcloudapi/wordcloudapp/dashboard.py
@@ -45,11 +45,9 @@ def clean_data(result):
 def jsondata(bus_id):
     """Query database, clean data and output final JSON response
     """
-    df1 = pd.read_csv('bussmall1.csv')
-    df2 = pd.read_csv('bussmall2.csv')
-    df = pd.concat([df1, df2])
+    df = pd.read_csv('bussmall1.csv')
     df = df[df['business_id'] == bus_id]
     cleaned = clean_data(df)
-    data = jsonify(data=cleaned)
+    data = cleaned.to_json(orient="records")
 
     return data
