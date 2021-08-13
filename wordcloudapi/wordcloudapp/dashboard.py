d73fd77cb20fe63a0ded3e4f8ca344681c47e307
Nayomi Chibana <nchibana@Nayomis-MacBook-Pro.local>
2021-08-13 01:56:35 -0700

    small dataset

--- wordcloudapi/wordcloudapp/dashboard.py
+++ wordcloudapi/wordcloudapp/dashboard.py
@@ -16,29 +16,29 @@ def create_dict(list1, list2, key1, key2):
 def clean_data(result):
     """Clean data and coerce into necessary data structure
     """
-    result[0]['competitors'] = ast.literal_eval(result[0]['competitors'].\
+    result.iloc[0]['competitors'] = ast.literal_eval(result.iloc[0]['competitors'].\
                                strip('"').strip("'").replace("\\\\",'').\
                                    replace("\\",''))
-    result[0]['bestinsector'] = ast.literal_eval(result[0]['bestinsector'].\
+    result.iloc[0]['bestinsector'] = ast.literal_eval(result.iloc[0]['bestinsector'].\
                                 strip('"').strip("'").\
                                 replace("\\\\",'').replace("\\",''))
-    starsovertime = ast.literal_eval(result[0]['avg_stars_over_time'].\
+    starsovertime = ast.literal_eval(result.iloc[0]['avg_stars_over_time'].\
                     strip("'").strip('"').replace("\\\\",'').\
                     replace("\\",''))
-    sentimentchunks = ast.literal_eval(result[0]['chunk_sentiment'].\
+    sentimentchunks = ast.literal_eval(result.iloc[0]['chunk_sentiment'].\
                       strip("'").strip('"').replace("\\\\",'').\
                       replace("\\",''))
-    result[0]['avg_stars_over_time'] = create_dict(starsovertime[1], \
+    result.iloc[0]['avg_stars_over_time'] = create_dict(starsovertime[1], \
                                       starsovertime[0], 'date', 'stars')
-    result[0]['chunk_sentiment'] = create_dict(sentimentchunks[0], \
+    result.iloc[0]['chunk_sentiment'] = create_dict(sentimentchunks[0], \
                                    sentimentchunks[1], 'chunks', 'sentiment')
-    countbystar = ast.literal_eval(result[0]['count_by_star'].strip('"'))
+    countbystar = ast.literal_eval(result.iloc[0]['count_by_star'].strip('"'))
     stars, starcounts = list(countbystar.keys()), list(countbystar.values())
-    result[0]['count_by_star'] = create_dict(stars, starcounts, \
+    result.iloc[0]['count_by_star'] = create_dict(stars, starcounts, \
                                 'stars', 'count')
-    reviewbyyear = ast.literal_eval(result[0]['review_by_year'].strip('"'))
+    reviewbyyear = ast.literal_eval(result.iloc[0]['review_by_year'].strip('"'))
     years, reviews = list(reviewbyyear.keys()), list(reviewbyyear.values())
-    result[0]['review_by_year'] = create_dict(years, reviews, 'year', 'total')
+    result.iloc[0]['review_by_year'] = create_dict(years, reviews, 'year', 'total')
     return result
     
 
